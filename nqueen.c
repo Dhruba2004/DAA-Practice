4a0474cdb27ddb700a8e37b9d6e3199f6f6a6f4b
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-04-13 00:37:52 +0530

    fix : code fix

--- nqueen.c
+++ nqueen.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 #include <math.h>
-// n is the number of queens
-// x is the global array to store the column no of the queen in the row i
-// j is the previous queen row no and k is next queen row no
-// x[j] is the previous queen column no and i is the next queen column no
-// Function prototype for place (to avoid implicit declaration warning)
+#define N 4
+
 int place(int k, int n, int* x);
-void nqueen(int k, int n, int* x, int board[4][4]);
-void printSolution(int board[4][4]);
+void nqueen(int k, int n, int* x, int board[N][N]);
+void printSolution(int board[N][N]);
 
 int place(int k, int n, int* x) {
-  int i;
-  for (int j = 1; j < k; j++) {
-    if ((x[j] == i) || abs(x[j] - i) == abs(j - k)) { // same column or same diagonal
+  for (int i = 0; i < k; i++) {
+    if ((x[i] == x[k]) || abs(x[i] - x[k]) == abs(i - k)) { // same column or same diagonal
       return 0;
     }
   }
   return 1;
 }
 
-void nqueen(int k, int n, int* x, int board[4][4]) {
-  if (place(k, n, x)) {
-    if (k == n) {
-      printSolution(board);
-    } else {
-      nqueen(k + 1, n, x, board); // same row checking
+void nqueen(int k, int n, int* x, int board[N][N]) {
+  for (int i = 0; i < n; i++) {
+    x[k] = i;
+    if (place(k, n, x)) {
+      if (k == n - 1) {
+        for (int j = 0; j < n; j++) {
+          board[j][x[j]] = 1;
+        }
+        printSolution(board);
+        for (int j = 0; j < n; j++) {
+          board[j][x[j]] = 0; // Reset the board for next solution
+        }
+      } else {
+        nqueen(k + 1, n, x, board);
+      }
     }
   }
 }
 
-void printSolution(int board[4][4]) {
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
+void printSolution(int board[N][N]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
       if (board[i][j]) {
         printf("Q ");
       } else {
@@ -40,13 +45,14 @@ void printSolution(int board[4][4]) {
     }
     printf("\n");
   }
+  printf("\n");
 }
 
 int main() {
-  int n = 4;
-  int x[n];
-  int board[4][4] = {{0}, {0}, {0}, {0}}; // Initialize board
+  int n = N;
+  int x[N] = {0}; // Initialize x array with 0s
+  int board[N][N] = {{0}}; // Initialize board
 
-  nqueen(1, n, x, board);
+  nqueen(0, n, x, board);
   return 0;
 }
